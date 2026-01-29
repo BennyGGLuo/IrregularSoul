59a1aaf7d23f03912bc5dc03eed258003d6ceca7
BennyGGLuo <bennyl_7431@hotmail.com>
2026-01-29 00:29:50 -0500

    Added 8 directional movement with corresponding animation

--- Assets/[Scripts]/PlayerMovement.cs
+++ Assets/[Scripts]/PlayerMovement.cs
@@ -3,30 +3,62 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d;
-    Vector3 movementVector;
-
+    Rigidbody2D rb;
     [SerializeField] float speed = 3f;
-
-    PlayerAnimation animate;
-
-    private void Awake()
+    Animator anim;
+    private Vector2 input;
+    private Vector2 lastMoveDirection;
+    //private bool facingLeft = true;
+    void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        movementVector = new Vector3();
-        animate = GetComponent<PlayerAnimation>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        movementVector.x = Input.GetAxis("Horizontal");
-        movementVector.y = Input.GetAxis("Vertical");
-
-        animate.horizontal = movementVector.x;
-
-        movementVector *= speed;
-
-        rigidbody2d.linearVelocity = movementVector;
+        ProcessInputs();
+        Animate();
+        //if (input.x < 0 && !facingLeft || input.x > 0 && facingLeft)
+        //{
+        //    Flip();
+        //}
     }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = input * speed;
+    }
+    void ProcessInputs()
+    {
+        // Store last move direction when we stop moving
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if ((moveX == 0 && moveY == 0) && (input.x != 0 || input.y != 0))
+        {
+            lastMoveDirection = input;
+        }
+
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        // Makes diagonal movement move the same rate as other movements
+        // otherwise diagonal would be faster
+        input.Normalize();
+    }
+    void Animate()
+    {
+        // Set animator parameters
+        anim.SetFloat("MoveX", input.x);
+        anim.SetFloat("MoveY", input.y);
+        anim.SetFloat("MoveMagnitude", input.magnitude);
+        anim.SetFloat("LastMoveX", lastMoveDirection.x);
+        anim.SetFloat("LastMoveY", lastMoveDirection.y);
+    }
+    //void Flip()
+    //{
+    //    Vector3 scale = transform.localScale;
+    //    scale.x *= -1; // Makes x negative >> flips sprite
+    //    transform.localScale = scale;
+    //    facingLeft = !facingLeft;
+    //}
 }
