5a287044966df423f486fb32885dd4b8a192ee38
BennyGGLuo <bennyl_7431@hotmail.com>
2026-03-07 19:12:29 -0500

    Added Store page in main menu for player upgrades like health and damage

---
+++ Assets/CoinsText.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CoinsText : MonoBehaviour
+{
+    [SerializeField] DataContainer dataContainer;
+    TMPro.TextMeshProUGUI text;
+
+    private void Start()
+    {
+        text = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Coins:" + dataContainer.coins.ToString();
+    }
+}
---
+++ Assets/PlayerUpgradeUIElement.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerUpgradeUIElement : MonoBehaviour
+{
+    [SerializeField] PlayerPersistentUpgrades upgrade;
+    [SerializeField] TextMeshProUGUI upgradeName;
+    [SerializeField] TextMeshProUGUI level;
+    [SerializeField] TextMeshProUGUI price;
+    [SerializeField] DataContainer dataContainer;
+
+    private void Start()
+    {
+        UpdateElement();
+    }
+
+    public void Upgrade()
+    {
+        PlayerUpgrades playerUpgrades = dataContainer.upgrades[(int)upgrade];
+
+        if (dataContainer.coins >= playerUpgrades.costToUpgrade)
+        {
+            dataContainer.coins -= playerUpgrades.costToUpgrade;
+            playerUpgrades.level += 1;
+            UpdateElement();
+        }
+    }
+
+    void UpdateElement()
+    {
+        PlayerUpgrades playerUpgrade = dataContainer.upgrades[(int)upgrade];
+
+        upgradeName.text = upgrade.ToString();
+        level.text = playerUpgrade.level.ToString();
+        price.text = playerUpgrade.costToUpgrade.ToString();
+    }
+}
